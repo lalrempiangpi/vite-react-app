451bf69cc015e1fedeffba23dd84b53904b26511
lalrempiangpi <124562293+lalrempiangpi@users.noreply.github.com>
2023-03-17 13:44:54 +0630

    We are changing ourself and the world

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
 //  You need to continue Baby
+const name = 'Lal Rem Piang';
 function Message() {
-  return <h1 className="container text-center ">Hello!</h1>;
+  return (
+    <h1 className="container text-center ">
+      Hello! {name} <br />
+      We can do a better life!
+    </h1>
+  );
 }
 
 export default Message;
